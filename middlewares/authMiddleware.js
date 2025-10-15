fc5e8acea6ada53d427adc09b83caacf49ba81a0
supershin69 <mgshinthant58@gmail.com>
2025-10-15 08:26:02 +0630

    fixed some stuff

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -7,23 +7,27 @@ const authMiddleware = async (req, res, next) => {
   try {
     const authHeader = req.headers.authorization;
     if (!authHeader || !authHeader.startsWith('Bearer ')) {
-      return res.status(401).json({ message: 'No token provided'});
-    }
+      return res.status(401).json({message: 'No header provided.'});
+    } 
 
     const token = authHeader.split(' ')[1];
+    if (!token) {
+      return res.status(401).json({ message: 'Not token provided'});
+    }
+
     const decoded = jwt.verify(token, JWT);
     const user = await User.findById(decoded.id);
-
     if (!user) {
       return res.status(404).json({ message: 'User not found'});
     }
 
     if (user.accessToken !== token) {
-      return res.status(403).json({ message: 'Forbidden'});
+      return res.status(403).json({ message: 'Not your token. Forbidden' });
     }
 
     req.user = user;
     next();
+    
   } catch(err) {
     return res.status(401).json({ message: 'Invalid token'});
   }
